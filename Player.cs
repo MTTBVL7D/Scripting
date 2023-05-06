a0330df6e7ab24afccabae88da0b6facde771375
MTTBVL7D <95588450+MTTBVL7D@users.noreply.github.com>
2023-05-06 20:40:43 +0900

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip damageSound;
     public int coins;
     private int health = 10;
+    public int maxHealth = 10;
     public GameObject fireballPrefab;
     public Transform attackPoint;
 
@@ -13,14 +17,25 @@ public class Player : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
-        print("Р—РґРѕСЂРѕРІСЊРµ РёРіСЂРѕРєР°:" + health);
+
+
+        if (health > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+            print("Здоровье игрока:" + health);
+        }
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(fireballPrefab,attackPoint. position, attackPoint.rotation);
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
         }
     }
 
@@ -29,3 +44,4 @@ public class Player : MonoBehaviour
         coins++;
     }
 }
+
